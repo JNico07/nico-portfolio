f8cbd4a7dd861dc5f4c4c80db34d85afc38d3e5e
nico <nicoolano43@gmail.com>
2025-01-08 19:37:23 +0800

    remove stars background on mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,19 @@ import { Points, PointMaterial } from '@react-three/drei'
 import * as random from 'maath/random/dist/maath-random.esm'
 
 const App = () => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
 
   return (
     <BrowserRouter>
 
       <div className="fixed inset-0 w-full h-full z-0 pointer-events-none">
+
       <Canvas
           camera={{ position: [0, 0, 1] }}
           dpr={Math.min(window.devicePixelRatio, 2)}
           gl={{ antialias: false }}
         >
-          <Stars />
+          {!isMobile && <Stars />}
       </Canvas>
 
       </div>
@@ -48,14 +50,9 @@ export default App
 function Stars(props) {
   const ref = useRef()
   const [sphere] = useState(() => random.inSphere(new Float32Array(1000), { radius: 1.5 }))
-
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
-
   useFrame((state, delta) => {
-    if (!isMobile) {
       ref.current.rotation.x -= delta / 10
       ref.current.rotation.y -= delta / 15
-    }
   })
 
   return (
